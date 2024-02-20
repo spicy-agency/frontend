743b2f5d188640928c55c038e4d9cfc7bb1c3514
Artyom Rak <ra44elo@gmail.com>
2024-02-20 02:11:02 +0100

    feat: add top-block on main page, fix font-weight for nav links

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,16 +3,20 @@ import {SharedModule} from '../shared/shared.module';
 import {LayoutComponent} from './layout.component';
 import {HeaderComponent} from './header/header.component';
 import {FooterComponent} from './footer/footer.component';
+import {MainComponent} from './main/main.component';
+import {RouterOutlet} from '@angular/router';
 
 const importsExports: any[] = [
   LayoutComponent,
   HeaderComponent,
   FooterComponent,
+  MainComponent,
 ];
 
 @NgModule({
   imports: [
     SharedModule,
+    RouterOutlet,
   ],
   declarations: [
     ...importsExports,
--- src/app/routes/routes.module.ts
+++ src/app/routes/routes.module.ts
@@ -2,13 +2,24 @@ import {NgModule} from '@angular/core';
 import {SharedModule} from '../shared/shared.module';
 import {RouterModule, Routes} from '@angular/router';
 import {LayoutComponent} from '../layout/layout.component';
-import {CommonModule} from '@angular/common';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
 import {BrowserModule} from '@angular/platform-browser';
+import {MainPageComponent} from './main-page/main-page.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        component: MainPageComponent,
+      },
+      {
+        path: '**',
+        redirectTo: '',
+      }
+    ],
   }
 ];
 
@@ -18,6 +29,10 @@ const routes: Routes = [
     RouterModule.forRoot(routes),
     CommonModule,
     BrowserModule,
+    NgOptimizedImage,
+  ],
+  declarations: [
+    MainPageComponent,
   ],
   exports: [
     RouterModule,
